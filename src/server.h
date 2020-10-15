7ef2371c7aa3157f6878d11d775c87a79187c184
su481ns8u <sghinmine54@gmail.com>
2020-10-15 09:45:30 +0530

    REFACTORED Code to use for chatroom

---
+++ src/server.h
@@ -0,0 +1,25 @@
+#ifndef LIST
+#define LIST
+
+typedef struct ClientNode
+{
+    int data;
+    struct ClientNode *prev;
+    struct ClientNode *link;
+    char ip[16];
+    char name[31];
+    char pass[31];
+} ClientList;
+
+ClientList *newNode(int sockfd, char *ip)
+{
+    ClientList *np = (ClientList *)malloc(sizeof(ClientList));
+    np->data = sockfd;
+    np->prev = NULL;
+    np->link = NULL;
+    strncpy(np->ip, ip, 16);
+    strncpy(np->name, "NULL", 5);
+    return np;
+}
+
+#endif // LIST
