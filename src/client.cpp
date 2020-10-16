96a8f91c241453c1b4c9f4056e9edcdc46a2330c
Vegirevathi <vegirevathi@gmail.com>
2020-10-16 10:47:21 +0530

    REFACTORED: Seperated client and main methods

--- src/client.cpp
+++ src/client.cpp
@@ -10,6 +10,7 @@
 #include <pthread.h>
 #include "string.cpp"
 #include "proto.h"
+#include "client.h"
 
 // Global variables
 volatile sig_atomic_t flag = 0;
@@ -55,13 +56,37 @@ void* recv_msg_handler(void* arg) {
   }
 }
 
-int main(int argc, char **argv)
+int Client::creatingSocket()
+{
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if ( sockfd == 0) 
+	{ 
+		perror("socket failed"); 
+		exit(EXIT_FAILURE); 
+	}
+	return sockfd;
+}
+
+void Client::connectingToServer(int sock)
 {
 	char *ip = "127.0.0.1";
-	int port = atoi(argv[1]);
+	int port = 8000;
 
-	signal(SIGINT, catch_ctrl_c_and_exit);
+	serverAddress.sin_family = AF_INET; 
+	serverAddress.sin_port = htons(port); 
+    serverAddress.sin_addr.s_addr = inet_addr(ip);
 
+    if (connect(sock, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) 
+    { 
+		perror("\nConnection Failed \n");  
+        exit(EXIT_FAILURE);
+	} else {
+		printf("\nConnected with server. Sending data...\n");
+	}
+}
+
+void Client::clientLogin(int sockfd)
+{
 	printf("Please enter your name: ");
 	fgets(name, 32, stdin);
 	str_trim_lf(name, strlen(name));
@@ -69,26 +94,9 @@ int main(int argc, char **argv)
 
 	if (strlen(name) > 32 || strlen(name) < 2){
 		printf("Name must be less than 30 and more than 2 characters.\n");
-		return EXIT_FAILURE;
+		exit(EXIT_FAILURE);
 	}
 
-	struct sockaddr_in server_addr;
-
-	/* Socket settings */
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = inet_addr(ip);
-	server_addr.sin_port = htons(port);
-
-
-  	// Connect to Server
-  	int err = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
- 	if (err == -1) {
-		printf("ERROR: connect\n");
-		return EXIT_FAILURE;
-	}
-
-	// Send name
 	send(sockfd, name, LENGTH_NAME, 0);
 
 	printf("=== WELCOME TO THE CHATROOM ===\n");
@@ -96,14 +104,25 @@ int main(int argc, char **argv)
 	pthread_t send_msg_thread;
   	if(pthread_create(&send_msg_thread, NULL, send_msg_handler, NULL) != 0){
 		printf("ERROR: pthread\n");
-    return EXIT_FAILURE;
+		exit(EXIT_FAILURE);
 	}
 
 	pthread_t recv_msg_thread;
   	if(pthread_create(&recv_msg_thread, NULL, recv_msg_handler, NULL) != 0){
 		printf("ERROR: pthread\n");
-		return EXIT_FAILURE;
+		exit(EXIT_FAILURE);
 	}
+}
+
+
+int main(int argc, char **argv)
+{
+	signal(SIGINT, catch_ctrl_c_and_exit);
+
+	Client client;
+	sockfd = client.creatingSocket();
+	client.connectingToServer(sockfd);
+	client.clientLogin(sockfd);
 
 	while (1){
 		if(flag){
